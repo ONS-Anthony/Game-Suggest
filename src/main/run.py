a215531308359e8de78c8558fee38afe2412e281
ONS-Anthony <achiu93@yahoo.co.uk>
2018-12-02 15:51:36 +0100

    Refactored with pep8 standards

--- src/main/run.py
+++ src/main/run.py
@@ -6,9 +6,11 @@ def data_setup():
         test_data = csv.DictReader(test_data_file)
         return test_data
 
+
 def get_user_input():
-    user_input = input("What are your interests? ")
-    return user_input
+    user_interest = input("What are your interests? ")
+    return user_interest
+
 
 if __name__ == "__main__":
     user_input = get_user_input()
