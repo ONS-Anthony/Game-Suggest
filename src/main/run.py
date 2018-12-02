9279e1b9c1d0544206063d187f46b74de9190cec
ONS-Anthony <achiu93@yahoo.co.uk>
2018-12-02 15:49:01 +0100

    Added method for getting user input

--- src/main/run.py
+++ src/main/run.py
@@ -6,6 +6,10 @@ def data_setup():
         test_data = csv.DictReader(test_data_file)
         return test_data
 
+def get_user_input():
+    user_input = input("What are your interests? ")
+    return user_input
 
 if __name__ == "__main__":
+    user_input = get_user_input()
     data = data_setup()
